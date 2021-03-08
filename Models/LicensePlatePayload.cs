e0d6bab5928781b06b938d89cacc94fef7205b66
ForceDestroy <sangeethan.thaventhiran@gmail.com>
2021-03-08 09:51:58 -0500

    Added Models folder/Created Model for LP Payload

---
+++ Models/LicensePlatePayload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenetecChallenge.N1
+{
+    class LicensePlatePayload
+    {
+        public DateTime LicensePlateCaptureTime { get; set; }
+        public string LicensePlate { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
